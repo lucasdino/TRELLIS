1498cd2d3ebbde18720e0f05c8e306e5d514bd2f
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-05-06 01:19:09 -0700

    minor bugs

--- onstart.sh
+++ onstart.sh
@@ -4,4 +4,7 @@ set -e
 source /opt/conda/etc/profile.d/conda.sh
 cd /opt/trellis
 ./setup.sh --new-env --basic --xformers --flash-attn --diffoctreerast --vox2seq --spconv --mipgaussian --kaolin --nvdiffrast --demo
+
+# Reload conda context in case a new env was just created
+source /opt/conda/etc/profile.d/conda.sh
 conda activate trellis
