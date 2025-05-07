07c1f6771c40eabb4939a3683589d376e6dead72
Lucas Dionisopoulos <lucasdionisopoulos@gmail.com>
2025-05-07 10:46:39 -0700

    trying new lite process

--- onstart.sh
+++ onstart.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 set -e
-set -x  # Enable debug mode to print each command before execution
+set -x
 
+# Add to python path for trellis
 export PYTHONPATH=/opt/trellis:$PYTHONPATH
 
 source /opt/conda/etc/profile.d/conda.sh
-conda activate base || { echo "[ERROR] Failed to activate conda environment 'base'"; exit 1; }
-
-cd /opt/trellis || { echo "[ERROR] Failed to change directory to /opt/trellis"; exit 1; }
+conda activate base
+cd /opt/trellis
 
 # Log the start of setup.sh execution
 echo "[INFO] Starting setup.sh script with all flags except HELP and NEW_ENV"
-./setup.sh --basic --xformers --flash-attn --diffoctreerast --vox2seq --spconv --mipgaussian --kaolin --nvdiffrast --demo || {
-    echo "[ERROR] setup.sh script failed";
-    exit 1;
-}
+# ./setup.sh --basic --xformers --flash-attn --diffoctreerast --vox2seq --spconv --mipgaussian --kaolin --nvdiffrast --demo || {
+#     echo "[ERROR] setup.sh script failed";
+#     exit 1;
+# }
 
 # Log successful completion
 echo "[INFO] onstart.sh completed successfully"
